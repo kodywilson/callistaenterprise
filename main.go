e77ebff203580d7b267e1619e77bfac55a90cf50
kodywilson <kodywilson@gmail.com>
2018-10-02 11:23:10 -0700

    added web server

--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/kodywilson/callistaenterprise/service"
 )
 
 var appName = "accountservice"
 
 func main() {
 	fmt.Printf("Starting %v\n", appName)
+	service.StartWebServer("6767")
 }
