bf5e5af51ef22108c72ab587aaf83d4604344190
kodywilson <kodywilson@gmail.com>
2018-10-03 14:16:06 -0700

    added healthchecker service

--- copyall.sh
+++ copyall.sh
@@ -4,7 +4,11 @@ export CGO_ENABLED=0
 
 go get;go build -o accountservice-linux-amd64;echo built `pwd`
 
+cd healthchecker;go get;go build -o healthchecker-linux-amd64;echo built `pwd`;cd ..
+
 export GOOS=darwin
+
+cp healthchecker/healthchecker-linux-amd64 .
 
 docker build -t kodywilson/accountservice .
 
